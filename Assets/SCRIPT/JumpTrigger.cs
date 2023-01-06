0af67cbb5dda5489c8eacd1cf7c1a3e2398f2b4f
HEKEPOIU <decnovpk92805@gmail.com>
2023-01-06 11:05:34 +0800

    16

--- Assets/SCRIPT/JumpTrigger.cs
+++ Assets/SCRIPT/JumpTrigger.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class JumpTrigger : MonoBehaviour
 {
     [SerializeField] float force;
+    [SerializeField] float x =1;
+    [SerializeField] float y =1;
     void OnTriggerStay2D(Collider2D other)
     {
         print("true");
-        other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1,1)*force*Time.deltaTime,ForceMode2D.Impulse);
+        other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(x,y)*force*Time.deltaTime,ForceMode2D.Impulse);
     }
 }
